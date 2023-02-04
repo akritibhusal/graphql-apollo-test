fa55d94876e8221a1751ccc23ecd22a21f48ebaa
Akriti Bhusal <akritibhusal@gmail.com>
2023-02-04 18:00:13 +1100

    change fetch request to graphql-request

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,9 @@
  * EXAMPLE ONLY
  * Needs a frontend library or postman to run
  */
+
+const request = require('graphql-request');
+
 const endpoint = 'http://localhost:4000';
 
 const query =  `
@@ -10,15 +13,22 @@ const query =  `
   }
 `
 
-const options = {
-  method: "POST",
-  headers: {"Content-Type": 'application/json'},
-  body: JSON.stringify({query})
-}
+/**
+ * Fetch replaced with graphql-request
+ */
+// const options = {
+//   method: "POST",
+//   headers: {"Content-Type": 'application/json'},
+//   body: JSON.stringify({query})
+// }
+// fetch(endpoint, options)
+//   .then(res => res.json())
+//   .then(({data}) => `totalDays:${data.totalDays}`)
+//   .then(console.log)
+//   .catch(console.error);
 
-fetch(endpoint, options)
-  .then(res => res.json())
-  .then(({data}) => `totalDays:${data.totalDays}`)
+console.log("Sending the query");
+request(endpoint, query)
+  .then(({totalDays}) => `totalDays: ${totalDays}`)
   .then(console.log)
   .catch(console.error);
-
